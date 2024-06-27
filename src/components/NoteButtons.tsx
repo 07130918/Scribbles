c55784a543e9b30d41f0e16cd49e02d547e4d0b5
Kota Sato <tokyosato1@gmail.com>
2024-06-27 17:09:58 +0900

    chore: Update NoteButtons component to use consistent indentation and button color scheme

--- src/components/NoteButtons.tsx
+++ src/components/NoteButtons.tsx
@@ -13,61 +13,59 @@ import { NoteIndexButtonWrapper } from "./ChakraExtension";
  * @property handleRemoveNote - A function that removes a notepad
  */
 type NoteButtonsProps = {
-    notes: Note[];
-    activeNoteIndex: number;
-    setActiveNoteIndex: (index: number) => void;
-    handleAddNote: () => void;
-    handleRemoveNote: () => void;
+	notes: Note[];
+	activeNoteIndex: number;
+	setActiveNoteIndex: (index: number) => void;
+	handleAddNote: () => void;
+	handleRemoveNote: () => void;
 };
 
 const NoteButtons = ({
-    notes,
-    activeNoteIndex,
-    setActiveNoteIndex,
-    handleAddNote,
-    handleRemoveNote
+	notes,
+	activeNoteIndex,
+	setActiveNoteIndex,
+	handleAddNote,
+	handleRemoveNote,
 }: NoteButtonsProps) => {
-    return (
-        <>
-            <Flex mb={2}>
-                <NoteIndexButtonWrapper>
-                    {[...Array(notes.length).keys()].map((index) => {
-                        return (
-                            <Button
-                                key={index}
-                                colorScheme="whatsapp"
-                                variant={index === activeNoteIndex ? "solid" : "outline"}
-                                mr={1}
-                                size="sm"
-                                onClick={() => setActiveNoteIndex(index)}
-                            >
-                                {index + 1}
-                            </Button>
-                        );
-                    })}
-                </NoteIndexButtonWrapper>
-                <Button
-                    colorScheme="twitter"
-                    variant="solid"
-                    size="sm"
-                    ml="auto"
-                    mr={1}
-                    onClick={handleAddNote}
-                >
-                    +
-                </Button>
-                <Button
-                    colorScheme="red"
-                    variant={notes.length <= NOTE_MIN_COUNT ? "outline" : "solid"}
-                    size="sm"
-                    disabled={notes.length <= NOTE_MIN_COUNT}
-                    onClick={handleRemoveNote}
-                >
-                    -
-                </Button>
-            </Flex>
-        </>
-    );
+	return (
+		<Flex mb={2}>
+			<NoteIndexButtonWrapper>
+				{[...Array(notes.length).keys()].map((index) => {
+					return (
+						<Button
+							key={index}
+							colorScheme="whatsapp"
+							variant={index === activeNoteIndex ? "solid" : "outline"}
+							mr={1}
+							size="sm"
+							onClick={() => setActiveNoteIndex(index)}
+						>
+							{index + 1}
+						</Button>
+					);
+				})}
+			</NoteIndexButtonWrapper>
+			<Button
+				colorScheme="pink"
+				variant="solid"
+				size="sm"
+				ml="auto"
+				mr={1}
+				onClick={handleAddNote}
+			>
+				+
+			</Button>
+			<Button
+				colorScheme="blue"
+				variant={notes.length <= NOTE_MIN_COUNT ? "outline" : "solid"}
+				size="sm"
+				disabled={notes.length <= NOTE_MIN_COUNT}
+				onClick={handleRemoveNote}
+			>
+				-
+			</Button>
+		</Flex>
+	);
 };
 
 export default NoteButtons;
