048c58a2214fdb9d27f606bcfe8ef4f073e7fccb
Kota Sato <tokyosato1@gmail.com>
2024-06-27 17:13:24 +0900

    chore: Update dependencies and add react-textarea-autosize package

--- src/components/ChakraExtension.tsx
+++ src/components/ChakraExtension.tsx
@@ -1,9 +1,10 @@
-import { Box, BoxProps, Flex, FlexProps } from "@chakra-ui/react";
+import type { BoxProps, FlexProps } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 export const BrowserExtension = (props: BoxProps) => (
-    <Box w='420px' p='10px' {...props} />
+	<Box w="420px" p="10px" {...props} />
 );
 
 export const NoteIndexButtonWrapper = (props: FlexProps) => (
-    <Flex maxW='315px' overflowX="scroll" {...props} />
+	<Flex maxW="315px" overflowX="scroll" {...props} />
 );
--- src/components/NoteField.tsx
+++ src/components/NoteField.tsx
@@ -1,4 +1,18 @@
-import { Box, Textarea } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import TextareaAutosize from "react-textarea-autosize";
+
+const StyledTextareaAutoSize = styled(TextareaAutosize)`
+    border-width: 1px;
+    border-radius: 0.375rem;
+    padding: 0.5rem;
+    border-color: inherit;
+    background-color: inherit;
+    &:focus {
+        border-color: var(--chakra-colors-blue-500);
+        box-shadow: 0 0 0 1px var(--chakra-colors-blue-500);
+    }
+`;
 
 /**
  * A type of component that is a text area that is displayed in Notepad
@@ -6,25 +20,21 @@ import { Box, Textarea } from "@chakra-ui/react";
  * @property onChange - A function that is called when the text in the text area changes
  */
 type NoteFieldProps = {
-    text: string;
-    onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+	text: string;
+	onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
 };
 
 const NoteField = ({ text, onChange }: NoteFieldProps) => {
-    return (
-        <>
-            <Box>
-                <Textarea
-                    value={text}
-                    placeholder="Enter memo here"
-                    resize='none'
-                    w='400px'
-                    h='250px'
-                    onChange={onChange}
-                />
-            </Box>
-        </>
-    );
+	return (
+		<Box w="400px">
+			<StyledTextareaAutoSize
+				value={text}
+				placeholder="Enter memo here"
+				minRows={10}
+				onChange={onChange}
+			/>
+		</Box>
+	);
 };
 
 export default NoteField;
